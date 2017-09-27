a4c8fbf7dedff146772ec057bbcf52f2493d1bc7
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2017-09-28 00:12:43 +0200

    detected the car on same lane and very suden change of speed

--- src/main.cpp
+++ src/main.cpp
@@ -255,6 +255,36 @@ int main() {
 
             int prev_size = previous_path_x.size();
 
+            //<process sensor fusion>
+            if (prev_size>0){
+                car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            //find ref_v to use
+            for (int i = 0; i< sensor_fusion.size();i++){
+              double d = sensor_fusion[i][6];
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx * vx + vy * vy);
+              double check_car_s_now = sensor_fusion[i][5];
+
+              // check the s in the future
+              double check_car_s = check_car_s_now + 0.02 * prev_size * check_speed;
+
+
+              //car is in my lane
+              if (d > (2 + 4 * lane - 2) && (d < (2 + 4 * lane + 2))) {
+                //check s values grater than mine and s gap
+                if ((check_car_s > car_s) && ((check_car_s - car_s) < 30) && (car_speed > check_speed)) {
+                  ref_vel = 29.5; //mph
+                  too_close = true;
+                }
+              }
+            }
+
+            //</process sensor fusion>
 
             // keep track of my reference state
             // eithe will reference the starting point as where the car is or at the previous path end point
